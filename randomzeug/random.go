2a6f2d4f24c230839ac6de0b7416b8933e887488
plombardi <plombardi89@gmail.com>
2019-03-20 00:42:33 -0400

    Move the stuff from plombardi89/rando into the randomzeug pkg

---
+++ randomzeug/random.go
@@ -0,0 +1,76 @@
+package randomzeug
+
+import (
+	"errors"
+	"math/rand"
+	"time"
+)
+
+// Random provides convenient utility funcs for generating random data
+type Random struct {
+	rand *rand.Rand
+}
+
+// NewRandom returns a new Random seeded with the current UTC time converted to nanoseconds.
+func NewRandom() *Random {
+	return NewSeededRandom(time.Now().UTC().UnixNano())
+}
+
+// NewSeededRandom returns a new Random seeded with the provided value.
+func NewSeededRandom(seed int64) *Random {
+	return &Random{rand: rand.New(rand.NewSource(seed))}
+}
+
+// RandomString returns a random string of the given length from a standard lowercase-only alphanumeric alphabet.
+func (r *Random) RandomString(length int) string {
+	return r.RandomStringUsingCustomAlphabet(length, []rune("0123456789abcdefghijklmnopqrstuvwxyz"))
+}
+
+// RandomStringUsingCustomAlphabet return a random string of the given length from a custom alphabet.
+func (r *Random) RandomStringUsingCustomAlphabet(length int, alphabet []rune) string {
+	res := make([]rune, length)
+	for i := range res {
+		res[i] = alphabet[r.rand.Intn(len(alphabet))]
+	}
+
+	return string(res)
+}
+
+// RandomSelectionFromStringSlice returns a random string from a given slice of strings.
+func (r *Random) RandomSelectionFromStringSlice(values []string) string {
+	if len(values) == 0 {
+		return ""
+	}
+
+	return values[rand.Intn(len(values))]
+}
+
+// RandomBool returns a random boolean value.
+func (r *Random) RandomBool() bool {
+	return rand.Intn(2) == 0
+}
+
+func (r *Random) SampleStringSlice(values []string, size int) ([]string, error) {
+	if size < 0 || size > len(values) {
+		return nil, errors.New("size is negative or greater than the the length of input values")
+	}
+
+	if size == 0 {
+		return []string{}, nil
+	}
+
+	sampled := make(map[string]struct{})
+	for i := size; i >= 0; i-- {
+		candidate := r.RandomSelectionFromStringSlice(values)
+		if _, found := sampled[candidate]; !found {
+			sampled[candidate] = struct{}{}
+		}
+	}
+
+	res := make([]string, 0)
+	for k := range sampled {
+		res = append(res, k)
+	}
+
+	return res, nil
+}
---
+++ randomzeug/random_test.go
@@ -0,0 +1,91 @@
+package randomzeug_test
+
+import (
+	"github.com/plombardi89/gozeug/randomzeug"
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"regexp"
+	"testing"
+	"time"
+)
+
+var seed int64 = 42
+
+// TestNewRandom ensures creating a Random without a specific seed yields two distinct generators at different times.
+func TestNewRandom(t *testing.T) {
+	r1 := randomzeug.NewRandom()
+
+	firstRun := make([]string, 0)
+	for i := 0; i < 1000; i++ {
+		firstRun = append(firstRun, r1.RandomString(5))
+	}
+
+	time.Sleep(1 * time.Second)
+	r2 := randomzeug.NewRandom()
+	secondRun := make([]string, 0)
+	for i := 0; i < 1000; i++ {
+		secondRun = append(secondRun, r2.RandomString(5))
+	}
+
+	assert.False(t, reflect.DeepEqual(firstRun, secondRun))
+}
+
+// TestNewSeededRandom ensures creating a Random with a specific seed yields the same random number generator.
+func TestNewSeededRandom(t *testing.T) {
+	r1 := randomzeug.NewSeededRandom(seed)
+
+	firstRun := make([]string, 0)
+	for i := 0; i < 1000; i++ {
+		firstRun = append(firstRun, r1.RandomString(5))
+	}
+
+	r2 := randomzeug.NewSeededRandom(seed)
+	secondRun := make([]string, 0)
+	for i := 0; i < 1000; i++ {
+		secondRun = append(secondRun, r2.RandomString(5))
+	}
+
+	assert.EqualValues(t, firstRun, secondRun)
+}
+
+func TestRandom_RandomString(t *testing.T) {
+	var table = []struct {
+		length  int
+		matches *regexp.Regexp
+	}{
+		{0, regexp.MustCompile("")},
+		{100, regexp.MustCompile("[a-z0-9]{100}")},
+	}
+
+	r := randomzeug.NewSeededRandom(seed)
+	for _, tt := range table {
+		v := r.RandomString(tt.length)
+		assert.Regexp(t, tt.matches, v)
+	}
+}
+
+func TestRandom_SampleStringSlice(t *testing.T) {
+	var table = []struct {
+		values     []string
+		sampleSize int
+		shouldErr  bool
+	}{
+
+		{[]string{"a", "b", "c", "d", "e", "e", "f", "e"}, 3, false},
+		{[]string{"a", "b", "c"}, 0, false},
+		{[]string{"a", "b", "c"}, -1, true},
+		{[]string{"a", "b", "c"}, 10, true},
+	}
+
+	r := randomzeug.NewSeededRandom(seed)
+	for _, tt := range table {
+		sampled, err := r.SampleStringSlice(tt.values, tt.sampleSize)
+
+		if tt.shouldErr {
+			assert.NotNil(t, err)
+			return
+		}
+
+		assert.Len(t, sampled, tt.sampleSize)
+	}
+}
